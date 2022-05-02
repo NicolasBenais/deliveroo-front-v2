c621f702eb7c8ffd8f3dc5a563ca5c4a0f3bfe1f
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-05-02 22:11:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://deliveroo-backend-nbns.herokuapp.com/"
+      );
+      setData(response.data);
+
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return isLoading === true ? (
+    <p>Loading</p>
+  ) : (
+    <div>
+      <header>
+        <div className="left-part-header">
+          <h1>{data.restaurant.name}</h1>
+          <p>{data.restaurant.description}</p>
+        </div>
+        <img src={data.restaurant.picture} alt="" />
       </header>
+      <div className="center">
+        {data.categories.map((items, index) => {
+          return (
+            <div className="MenuItems">
+              <h2 key={index}> {items.name} </h2>
+              <div className="MenuItem">
+                {items.meals.map((item) => {
+                  return (
+                    <div className="items">
+                      <div key={item.id} className="item">
+                        <div className="left-part-item">
+                          <h3>{item.title}</h3>
+                          <p className="item-description">{item.description}</p>
+                          <p>{item.price}</p>
+                        </div>
+                        <img src={item.picture} alt="" />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
