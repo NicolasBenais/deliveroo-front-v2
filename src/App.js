63011ac3b8a50759fb18096815226ee9e4ffc840
Nicolas BENAIS <nicolasbenais@MacBook-Air-de-Nicolas.local>
2022-05-03 12:58:14 +0200

    issue for update quantity in cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function App() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +19,34 @@ function App() {
     };
     fetchData();
   }, []);
+
+  const addToCart = (item) => {
+    let quantity = 1;
+    const newCart = [...cart];
+    for (let i = 0; i <= newCart.length; i++) {
+      console.log(cart.length);
+      if (newCart.length === 0) {
+        newCart.push({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          quantity: quantity++,
+        });
+        setCart(newCart);
+      } else if (newCart[i].id !== item.id) {
+        newCart.push({
+          id: item.id,
+          title: item.title,
+          price: item.price,
+          quantity: quantity++,
+        });
+        setCart(newCart);
+      } else {
+        newCart.quantity = quantity++;
+        setCart(newCart);
+      }
+    }
+  };
 
   return isLoading === true ? (
     <p>Loading</p>
@@ -56,27 +85,47 @@ function App() {
         {/* Menu Title */}
         {data.categories.map((items, index) => {
           return (
-            <div className="MenuItems">
-              <h2 key={index}> {items.name} </h2>
+            items.meals.length > 0 && (
+              <div className="MenuItems">
+                <h2 key={index}> {items.name} </h2>
 
-              {/* Item */}
-              <div className="MenuItem">
-                {items.meals.map((item) => {
-                  return (
-                    <div key={item.id} className="item">
-                      <div className="left-part-item">
-                        <h3>{item.title}</h3>
-                        <p className="item-description">{item.description}</p>
-                        <p className="price">{item.price} €</p>
+                {/* Item */}
+                <div className="MenuItem">
+                  {items.meals.map((item) => {
+                    return (
+                      <div
+                        onClick={() => {
+                          addToCart(item);
+                        }}
+                        key={item.id}
+                        className="item"
+                      >
+                        <div className="left-part-item">
+                          <h3>{item.title}</h3>
+                          <p className="item-description">{item.description}</p>
+                          <p className="price">{item.price} €</p>
+                        </div>
+                        {item.picture && <img src={item.picture} alt="" />}
                       </div>
-                      {item.picture && <img src={item.picture} alt="" />}
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
-            </div>
+            )
           );
         })}
+        <div className="cart">
+          <button>Valider mon panier</button>
+          {cart.map((cartItem) => {
+            return (
+              <div>
+                <div>
+                  {cartItem.quantity} {cartItem.title} {cartItem.price} €
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
